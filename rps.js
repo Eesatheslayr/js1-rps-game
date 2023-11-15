d5e6014ba74aea5c3aff61c96c9133b01c0dc0ff
Eesatheslayr <eesag4@gmail.com>
2023-11-15 13:05:56 +0200

    Added the fucntion that plays one round of RPS
    I created the function playRound that takes the parameters of:
    playerChoice and computerChoice. After adding the conditional statements,
    the game plays as intended.

--- rps.js
+++ rps.js
@@ -4,15 +4,15 @@ let scissors
 
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random()*3)
-    if (computerChoice===1){
+    if (computerChoice===0){
       return'rock';
-    } else if(computerChoice===2){
+    } else if(computerChoice===1){
       return 'paper';
-    } else (computerChoice===3)
+    } else (computerChoice===2)
       return 'scissors';
   }
   
- function getPlayerChoice(){
+ /*function getPlayerChoice(){
    const playerChoice= prompt('ROCK, PAPER OR SCISSORS?').toLowerCase();
    if(playerChoice==='rock'){
    return 'You Chose Rock!';
@@ -20,9 +20,26 @@ function getComputerChoice(){
     return 'You Chose Paper!';
    }  else if (playerChoice==='scissors')
     return 'You Chose Scissors!';
+} */
+
+function playRound(playerChoice, computerChoice){
+    if (playerChoice==='Rock'&& computerChoice===2){/*rock beats scissors*/
+    return 'You Win! Rock beats Scissors.';
+    } else if(playerChoice==='Rock' && computerChoice===1){/*paper beats rock*/
+        return 'You Lose! Paper beats Rock'
+    } else if(playerChoice==='Paper'&& computerChoice===0){/*paper beats rock*/
+       return 'You Win! Paper beats Rock'
+    } else if (playerChoice==='Paper' && computerChoice===2){/*scissors beats paper*/ 
+        return 'You Lose! Scissors beats Paper'
+    } else if (playerChoice==='Scissors' && computerChoice===1){ /*scissors beats paper*/
+        return 'You win! Scissors beats Paper'
+    } else if (playerChoice==='Scissors' && computerChoice===0){ /* rock beats scissors*/
+        return 'You Lose! Rock beats Scissors'
+    } else if(playerChoice==='Rock'&& computerChoice===0){
+        return 'Its a Draw!'
+    } else if(playerChoice==='Paper'&& computerChoice===1){
+        return 'Its a Draw!'
+    } else if(playerChoice==='Scissors' && computerChoice===2){
+        return 'Its a Draw!'
+    }
 }
-
-
-    
- 
- 
