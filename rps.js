380c19ab16658278fcfffb4bdd8ff047d3ab4fa4
Eesatheslayr <eesag4@gmail.com>
2023-11-21 23:34:06 +0200

    "Add getScore() and fullGame() functions
    Introduce getScore() function to calculate and display the final score after a 5-round Rock Paper Scissors game. Additionally, implement fullGame() function to orchestrate five rounds of the game() function and conclude with a call to getScore(). Enhances overall gameplay experience and provides a clear winner announcement at the end of the game."

--- rps.js
+++ rps.js
@@ -1,8 +1,8 @@
 let rock;
 let paper;
 let scissors;
-let playerScore=0;
-let computerScore=0;
+let playerScore=1;
+let computerScore=1;
 
 
 function getComputerChoice(){
@@ -15,97 +15,46 @@ function getComputerChoice(){
       return 'scissors';
   }
  
- function getPlayerChoice(){
-   let playerChoice = prompt('ROCK, PAPER OR SCISSORS?').toLowerCase();
-   if(playerChoice==='rock'){
-   return 'Rock!';
-   } else if(playerChoice==='paper'){
-    return 'Paper!';
-   }  else if (playerChoice==='scissors')
-    return 'Scissors!';
-}
 
 function playRound(playerChoice=prompt('ROCK, PAPER OR SCISSORS?').toLowerCase, computerChoice){
-    if (playerChoice==='Rock'&& computerChoice===2){
+    if (playerChoice==='rock'&& computerChoice===2){
     return 'You Win! Rock beats Scissors.';
-    } else if(playerChoice==='Rock' && computerChoice===1){
+    } else if(playerChoice==='rock' && computerChoice===1){
         return 'You Lose! Paper beats Rock'
-    } else if(playerChoice==='Paper'&& computerChoice===0){
+    } else if(playerChoice==='paper'&& computerChoice===0){
        return 'You Win! Paper beats Rock'
-    } else if (playerChoice==='Paper' && computerChoice===2){ 
+    } else if (playerChoice==='paper' && computerChoice===2){ 
         return 'You Lose! Scissors beats Paper'
-    } else if (playerChoice==='Scissors' && computerChoice===1){ 
+    } else if (playerChoice==='scissors' && computerChoice===1){ 
         return 'You win! Scissors beats Paper'
-    } else if (playerChoice==='Scissors' && computerChoice===0){ 
+    } else if (playerChoice==='scissors' && computerChoice===0){ 
         return 'You Lose! Rock beats Scissors'
-    } else if(playerChoice==='Rock'&& computerChoice===0){
+    } else if(playerChoice==='rock'&& computerChoice===0){
         return 'Its a Draw!'
-    } else if(playerChoice==='Paper'&& computerChoice===1){
+    } else if(playerChoice==='paper'&& computerChoice===1){
         return 'Its a Draw!'
-    } else if(playerChoice==='Scissors' && computerChoice===2){
+    } else if(playerChoice==='scissors' && computerChoice===2){
         return 'Its a Draw!'
     }   
 } 
 
-
-
-/*
-function playRound(playerChoice, computerChoice){
-    switch (playerChoice, computerChoice){
-        case playerChoice==='Rock'&& computerChoice===2:
-        case playerChoice==='Paper' && computerChoice===0:
-        case playerChoice==='Scissors' && computerChoice===1:
-            return 'You Win!'
-            break;
-
-        case playerChoice==='Rock'&& computerChoice===1:
-        case playerChoice==='Paper'&& computerChoice===2:
-        case playerChoice==='Scissors'&& computerChoice===0:
-            return 'You Lose!'
-            break;
-        
-        default:
-            return 'Tie Game!'              
-    }         
-}    */    
+function getScore(){
+    if (playerScore===5 || playerScore===4 || playerScore===3 && computerScore===0||computerScore===1||computerScore===2){
+        return 'You Win!'
+    } else if(computerScore===5 || computerScore===4 || computerScore===3 && playerScore===0 || playerScore===1 || playerScore===2){
+        return 'You Lose, Try Again!'
+    }
+}
 
 
 function game(){
-
 let computerChoice= Math.floor(Math.random()*3); 
-let playerChoice=prompt('ROCK, PAPER OR SCISSORS?')
+let playerChoice=prompt('ROCK, PAPER OR SCISSORS?').toLocaleLowerCase();
     switch (playRound(playerChoice, computerChoice)){
         case 'You Win! Rock beats Scissors.':
         case 'You Win! Paper beats Rock':
         case 'You win! Scissors beats Paper':       
-        return 'You Win!'                                       
-        break;
-
-        case 'You Lose! Paper beats Rock':
-        case 'You Lose! Scissors beats Paper':
-        case 'You Lose! Rock beats Scissors':
-        return 'You Lose!'
-        break;
-
-        default:
-        return 'Tie Game.'
-        }
-  }
-  console.log(game());
-  console.log(game());
-  console.log(game());
-  console.log(game());
-  console.log(game());
-  
-
-
-/*
-function getScore(){
-    switch (game()){
-        case 'You Win! Rock beats Scissors.':
-        case 'You Win! Paper beats Rock':
-        case 'You win! Scissors beats Paper':       
-        return playerScore++                                   
+        return playerScore++                                
         break;
 
         case 'You Lose! Paper beats Rock':
@@ -114,9 +63,18 @@ function getScore(){
         return computerScore++
         break;
 
-        default:
-        return 'Tie Game.'
-        }
+        default: 'Its a Draw!'
+        return game();    
     }
-*/
+  }
+  
+ function fullGame(){
+    console.log(game());
+    console.log(game());
+    console.log(game());
+    console.log(game());
+    console.log(game());
+    console.log(getScore());
+ }
 
+ fullGame();
