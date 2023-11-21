fc50c8ff20ca46f34cb425d0520659c6b7686230
Eesatheslayr <eesag4@gmail.com>
2023-11-21 09:51:07 +0200

    Reworked game() & playRound()
    I chnaged the parameter of my playRound() to incude the prompt'Rock, Paper
    or Scissors?'
    I also included the prompt in the variable playerChoice and defined it
    together with  computerChoice variable in the game() function.
    This fixed an issue i was having; when calling the game() function more
    than once it was giivng me the same computerChoice and using the same
    playerChoice from the first call.

--- rps.js
+++ rps.js
@@ -1,9 +1,12 @@
-let rock
-let paper
-let scissors
+let rock;
+let paper;
+let scissors;
+let playerScore=0;
+let computerScore=0;
+
 
 function getComputerChoice(){
-    let computerChoice = Math.floor(Math.random()*3)
+    let computerChoice= Math.floor(Math.random()*3); 
     if (computerChoice===0){
       return'rock';
     } else if(computerChoice===1){
@@ -11,29 +14,29 @@ function getComputerChoice(){
     } else (computerChoice===2)
       return 'scissors';
   }
- /*
+ 
  function getPlayerChoice(){
    let playerChoice = prompt('ROCK, PAPER OR SCISSORS?').toLowerCase();
    if(playerChoice==='rock'){
-   return 'You Chose Rock!';
+   return 'Rock!';
    } else if(playerChoice==='paper'){
-    return 'You Chose Paper!';
+    return 'Paper!';
    }  else if (playerChoice==='scissors')
-    return 'You Chose Scissors!';
-} */
+    return 'Scissors!';
+}
 
-function playRound(playerChoice, computerChoice){
-    if (playerChoice==='Rock'&& computerChoice===2){/*rock beats scissors*/
+function playRound(playerChoice=prompt('ROCK, PAPER OR SCISSORS?').toLowerCase, computerChoice){
+    if (playerChoice==='Rock'&& computerChoice===2){
     return 'You Win! Rock beats Scissors.';
-    } else if(playerChoice==='Rock' && computerChoice===1){/*paper beats rock*/
+    } else if(playerChoice==='Rock' && computerChoice===1){
         return 'You Lose! Paper beats Rock'
-    } else if(playerChoice==='Paper'&& computerChoice===0){/*paper beats rock*/
+    } else if(playerChoice==='Paper'&& computerChoice===0){
        return 'You Win! Paper beats Rock'
-    } else if (playerChoice==='Paper' && computerChoice===2){/*scissors beats paper*/ 
+    } else if (playerChoice==='Paper' && computerChoice===2){ 
         return 'You Lose! Scissors beats Paper'
-    } else if (playerChoice==='Scissors' && computerChoice===1){ /*scissors beats paper*/
+    } else if (playerChoice==='Scissors' && computerChoice===1){ 
         return 'You win! Scissors beats Paper'
-    } else if (playerChoice==='Scissors' && computerChoice===0){ /* rock beats scissors*/
+    } else if (playerChoice==='Scissors' && computerChoice===0){ 
         return 'You Lose! Rock beats Scissors'
     } else if(playerChoice==='Rock'&& computerChoice===0){
         return 'Its a Draw!'
@@ -41,27 +44,79 @@ function playRound(playerChoice, computerChoice){
         return 'Its a Draw!'
     } else if(playerChoice==='Scissors' && computerChoice===2){
         return 'Its a Draw!'
-    }
-}
-let playerChoice='Rock'
-let computerChoice = Math.floor(Math.random()*3)
+    }   
+} 
+
+
+
+/*
+function playRound(playerChoice, computerChoice){
+    switch (playerChoice, computerChoice){
+        case playerChoice==='Rock'&& computerChoice===2:
+        case playerChoice==='Paper' && computerChoice===0:
+        case playerChoice==='Scissors' && computerChoice===1:
+            return 'You Win!'
+            break;
+
+        case playerChoice==='Rock'&& computerChoice===1:
+        case playerChoice==='Paper'&& computerChoice===2:
+        case playerChoice==='Scissors'&& computerChoice===0:
+            return 'You Lose!'
+            break;
+        
+        default:
+            return 'Tie Game!'              
+    }         
+}    */    
+
 
 function game(){
+
+let computerChoice= Math.floor(Math.random()*3); 
+let playerChoice=prompt('ROCK, PAPER OR SCISSORS?')
     switch (playRound(playerChoice, computerChoice)){
         case 'You Win! Rock beats Scissors.':
         case 'You Win! Paper beats Rock':
         case 'You win! Scissors beats Paper':       
-        return '1-0'
+        return 'You Win!'                                       
         break;
 
         case 'You Lose! Paper beats Rock':
         case 'You Lose! Scissors beats Paper':
         case 'You Lose! Rock beats Scissors':
-        return '0-1'
+        return 'You Lose!'
         break;
 
         default:
-        return '1-1'
+        return 'Tie Game.'
+        }
+  }
+  console.log(game());
+  console.log(game());
+  console.log(game());
+  console.log(game());
+  console.log(game());
+  
+
+
+/*
+function getScore(){
+    switch (game()){
+        case 'You Win! Rock beats Scissors.':
+        case 'You Win! Paper beats Rock':
+        case 'You win! Scissors beats Paper':       
+        return playerScore++                                   
+        break;
+
+        case 'You Lose! Paper beats Rock':
+        case 'You Lose! Scissors beats Paper':
+        case 'You Lose! Rock beats Scissors':
+        return computerScore++
+        break;
+
+        default:
+        return 'Tie Game.'
+        }
     }
-}
-console.log(game());
+*/
+
